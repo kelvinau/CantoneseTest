b320ecfdc19aa4b59ad79b222245820b24baffab
kelvinau <kelvinau4413@hotmail.com>
2013-11-30 16:16:18 -0800

    Update check.php

--- check.php
+++ check.php
@@ -1,3 +1,14 @@
 <?
-$result = $_POST['submit']
+$total = 2;
+$result = 0;
+
+$hi = $_POST['hi'];
+if ($hi == "nei5 hou2")
+  $result++; 
+  
+$sleep = $_POST['sleep'];
+if ($hi == "ngo5 soeng2 fan3 gaau3")
+  $result++;
+
+echo "Result: ".($result/$total)*100."%";
 ?>
